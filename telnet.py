019411fc79dc7ac1921091b6ec0c4d8443447eea
Kaushik Ray <ray.1_1@live.com>
2016-11-02 15:57:54 -0400

    Add files via upload

---
+++ telnet.py
@@ -0,0 +1,38 @@
+import getpass
+import sys
+import telnetlib
+import time 
+import StringIO
+import random
+import array
+import string
+
+
+val = 100
+itte = 200000000
+PROMPT = ']'
+
+list_1 = [random.randrange(65,90) for c in range(val)]
+list_1.insert(val, ']')
+str2 = ''.join(str(e) for e in list_1)
+
+HOST = "192.168.2.235"
+
+tn = telnetlib.Telnet(HOST,23)
+
+count = 0
+error = 0
+while count<itte:
+    tn.write("%s" %str2)
+    str1 = tn.read_until(PROMPT,5)
+    count = count + 1
+    if str1 != str2:
+       error = error + 1
+       tn.read_until(PROMPT,1)
+    if count % 100 == 0:
+        print count, 
+        print " ",
+        print error
+    
+print ("\nTotal number of errors out of 2000 are: %i"  %error)    
+tn.close
